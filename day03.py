e0a351a7d0b4285b1d206c9390025182306ced3a
Àlex Costa Sánchez <alex.costasanchez@gmail.com>
2021-02-15 20:52:44 +0100

    Create day03.py

--- day03.py
+++ day03.py
@@ -1,24 +1,30 @@
-filename="input3.txt"
+filename = "input03.txt"
 
-def countTrees(lines, columnStep = 3, rowStep = 1):
-    totalTrees = 0
-    numRows = len(lines)
-    numCols = len(lines[0])
-    for i in range(0,int((numRows)/rowStep)):
-        square = lines[i * rowStep][(i * columnStep) % numCols]
-        if(square == '#'):
-            totalTrees += 1
-    return totalTrees
+
+def count_trees(lines, column_step = 3, row_step = 1):
+    total_trees = 0
+    num_rows = len(lines)
+    num_cols = len(lines[0])
+    for i in range(0, int(num_rows / row_step)):
+        square = lines[i * row_step][(i * column_step) % num_cols]
+        if square == '#':
+            total_trees += 1
+    return total_trees
+
+
+def count_trees_multiple_slopes(lines, slopes):
+    total = 1
+    for slope in slopes:
+        total *= count_trees(lines, slope[0], slope[1])
+    return total
+
 
 with open(filename) as f:
     input = f.readlines()
 input = [x.strip() for x in input]
 
-print('First part:', countTrees(input))
+print('First part:', count_trees(input))
 
 slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-total = 1
-for slope in slopes:
-    total *= countTrees(input, slope[0], slope[1])
 
-print('Second part:', total)
+print('Second part:', count_trees_multiple_slopes(input, slopes))
