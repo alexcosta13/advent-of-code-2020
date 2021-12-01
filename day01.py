f65a5d50038ecbfed2194d938eb2624e06a20a2a
Àlex Costa Sánchez <alex.costasanchez@gmail.com>
2021-12-01 09:13:48 +0100

    refractor: day01

--- day01.py
+++ day01.py
@@ -1,14 +1,14 @@
-filename = "input01.txt"
+filename = "inputs/input01.txt"
 
 
-def findSum(l, total=2020):
+def find_sum(l, total=2020):
     for i in range(0, len(l)):
         for j in range(i, len(l)):
             if l[i] + l[j] == total:
                 return l[i] * l[j]
 
 
-def findSumThree(l, total=2020):
+def find_sum_three(l, total=2020):
     for i in range(0, len(l)):
         for j in range(i, len(l)):
             for k in range(i, len(l)):
@@ -20,6 +20,6 @@ with open(filename) as f:
     input = f.readlines()
 input = [int(x.strip()) for x in input]
 
-print("First part:", findSum(input))
+print("First part:", find_sum(input))
 
-print("Second part:", findSumThree(input))
+print("Second part:", find_sum_three(input))
